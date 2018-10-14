dd57f502b9b445e03679f648763bc9b1c2c3ef93
Lutz Eichler <github@frickler24.de>
2018-10-14 18:23:39 +0200

    Add export functions for TblGesamt
    Fix some bugs;
    New admin/base.html to load css-file;
    Add some comments;

--- RechteDB/rapp/resources.py
+++ RechteDB/rapp/resources.py
@@ -4,12 +4,44 @@ Some Ressources for handling csv and Excel sheets
 """
 
 from import_export import resources
-from .models import Tblrechteneuvonimport
+from .models import Tblrechteneuvonimport, TblGesamt
+
 
 class MeinCSVImporterModel(resources.ModelResource):
+	"""
+		Resource-Class für import/export neuer Daten (erster Versuch, nur zum Export nutzen)
+	"""
 	class Meta:
 		model = Tblrechteneuvonimport
 		#encoding = "utf-8"
 		delimiter = ";"
-		sortable_by = ['nachname', '-identitaet', ]
+
+class GesamtExporterModel(resources.ModelResource):
+	"""
+		Resource-Class für export der Gesamttabelle (nach Selektion)
+	"""
+	class Meta:
+		model = TblGesamt
+		#encoding = "utf-8"
+		delimiter = ";"
+		fields = (
+			'id', 'userid_name',
+			'userid_name__name', 'userid_name__userid',
+
+			'tf', 'tf_beschreibung', 'plattform', 'plattform__tf_technische_plattform', 'direct_connect',
+			'enthalten_in_af', 'gf',
+
+			'af_gueltig_ab', 'af_gueltig_bis',
+			'af_zuweisungsdatum', 'datum',
+
+			'tf_kritikalitaet', 'tf_eigentuemer_org',
+			'vip_kennzeichen', 'zufallsgenerator',
+			'hoechste_kritikalitaet_tf_in_af', 'gf_beschreibung',
+			'gefunden', 'wiedergefunden', 'geaendert', 'neueaf', 'nicht_ai',
+			'patchdatum', 'wertmodellvorpatch', 'geloescht', 'loeschdatum',
+
+			'modell', 'modell__name_af_neu', 'modell__name_gf_neu',
+		)
+
+		export_order = fields
 
