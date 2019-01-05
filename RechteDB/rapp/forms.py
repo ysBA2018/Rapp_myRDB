e01f35647a8a8a93d2ca6200cad80345d58fa61e
Lutz Eichler <github@frickler24.de>
2019-01-05 19:36:00 +0100

    fixed issue3: Kein User vorg. bei Hinzufügen Rolle
    Zum Beheben des Fehlers wurde eine separate Form-Definition geschrieben.
    In der werden zwei verschiedene Werte behandelt:
    - userid    (nur die xv-nummer)
    - username  (die XV-Nummer und der Name)
    
    übner die View und das Context-Hash werden die beiden Werte an
    das Prozessing der HTML-Seite übergeben.
    
    Zusätzlich wurden einige kleinere Tippfehler bereinigt,
    die bei der Analyse gefunden wurden.
    
    Nicht mehr benötigte udn lange auskommentierte Statements
    wurden ebenfalls entfernt.

--- RechteDB/rapp/forms.py
+++ RechteDB/rapp/forms.py
@@ -1,12 +1,30 @@
 from django import forms
 from .models import TblUserIDundName, TblUserhatrolle, hole_organisationen
 
+# Das hätte man auch einfacher haben können, indem die relevanten Infos in views.py eingetragen worden wären
 class ShowUhRForm(forms.ModelForm):
 	class Meta:
 		model = TblUserhatrolle
 		fields = ['userid', 'rollenname', 'schwerpunkt_vertretung', 'bemerkung', ]
 
+# Hier ist das anders, weil zwei Methoden zur Klasse hinzugekommen sind
+# Initialisiere das Input Formular für neue Rolleneinträge mit der UserID
+class CreateUhRForm(forms.ModelForm):
+	userid = "Keine ID"
 
+	class Meta:
+		model = TblUserhatrolle
+		fields = ['userid', 'rollenname', 'schwerpunkt_vertretung', 'bemerkung', ]
+
+	def __init__(self, *args, **kwargs):
+		self.userid = kwargs.pop('userid', None)
+		super(CreateUhRForm, self).__init__(*args, **kwargs)
+
+		# assign the default userID to the choice field
+		self.initial['userid'] = self.userid
+		#self.fields['userid'].selection = self.userid
+
+#Auch hier ist das Thema das Initialisieren des Organisations-Choicefields
 class ImportForm(forms.Form):
 	# Die ersten Parameter, die für einen CSV-Import abgefragt werden müssen
 	organisation = forms.ChoiceField(label='Organisation')
@@ -16,7 +34,7 @@ class ImportForm(forms.Form):
 		super(ImportForm, self).__init__(*args, **kwargs)
 		self.fields['organisation'].choices = hole_organisationen()
 
-
+# Das hier behandelte boolean Field ist nicht Inhalt des Models, sondern ändert lediglich den Workflow
 class ImportForm_schritt3(forms.Form):
 	# Der Abschluss des zweiten Schritts besteht ebenfalls nur aus einer Bestätigung,
 	# deshalb sind auch hier keine Datenfelder angegeben
