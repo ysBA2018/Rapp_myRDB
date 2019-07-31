478b577919289b23412dbf1816b59f1b8257653c
Lutz Eichler <github@frickler24.de>
2019-07-31 23:19:57 +0200

    add first functions for export UhR table

--- RechteDB/rapp/view_UserHatRolle.py
+++ RechteDB/rapp/view_UserHatRolle.py
@@ -780,3 +780,61 @@ def panel_UhR_matrix_csv(request, flag = False):
 
 	return response
 
+def panel_UhR_af_export(request, id):
+	"""
+	Exportfunktion für das Filter-Panel aus der "User und Rollen"-Tabelle).
+	:param request: GET Request vom Browser
+	:return: Gerendertes HTML mit den CSV-Daten oder eine Fehlermeldung
+	"""
+	if request.method != 'GET':
+		return HttpResponse("Fehlerhafte CSV-Generierung in panel_UhR_af_export")
+
+	(namen_liste, panel_filter, rollen_liste, rollen_filter) = UhR_erzeuge_listen_ohne_rollen(request)
+	(userHatRolle_liste, selektierter_name, userids, usernamen,
+	 selektierte_haupt_userid, selektierte_userids, afmenge, afmenge_je_userID) \
+		= UhR_hole_daten(namen_liste, id)
+
+	"""
+	context = {
+		'paginator': paginator, 'pages': pages, 'pagesize': pagesize,
+		'filter': panel_filter, 'form': form,
+		'rollen_liste': rollen_liste, 'rollen_filter': rollen_filter,
+		'userids': userids, 'usernamen': usernamen, 'afmenge': afmenge,
+		'userHatRolle_liste': userHatRolle_liste,
+		'id': id,
+		'selektierter_name': selektierter_name,
+		'selektierte_userid': selektierte_haupt_userid,
+		'selektierte_userids': selektierte_userids,
+		'afmenge_je_userID': afmenge_je_userID,
+		'version': version,
+	}
+	"""
+	response = HttpResponse(content_type="text/csv")
+	response['Content-Disposition'] = 'attachment; filename="rollen.csv"'
+	response.write(u'\ufeff'.encode('utf8'))  # BOM (optional...Excel needs it to open UTF-8 file properly)
+
+	headline = [
+		smart_str(u'Name'),
+		smart_str(u'Rollenname'),
+		smart_str(u'AF'),
+		smart_str(u'Mussrecht')
+	]
+	for userid in selektierte_userids:
+		headline.append(smart_str(userid))
+	print (headline)
+
+	writer = csv.writer(response, csv.excel, delimiter = ',', quotechar = '"')
+	writer.writerow(headline)
+
+	for rolle in userHatRolle_liste:
+		for rollendefinition in TblRollehataf.objects.filter(rollenname = rolle.rollenname):
+			line = [selektierter_name, rolle.rollenname, rollendefinition.af]
+			if rollendefinition.mussfeld > 0: line.append('ja')
+			else: line.append('nein')
+			for userid in selektierte_userids:
+				print(line, userid)
+				if str(rollendefinition.af) in afmenge_je_userID[userid]: line.append('ja')
+				else: line.append('nein')
+			writer.writerow(line)
+
+	return response
